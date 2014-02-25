9c4ce48ff9fc5490109980e2aa19ada7d8c01f71
David Harrison <davidjohnharrison2@gmail.com>
2014-02-25 05:46:51 +0000

    install.sh - Fix bug whereby installing twice will place a symlink of a folder inside that folder (plus improved logging)
    So, for example, installing once would create the symlink ~/.vim -> $DIR/vim,
    and installing a second time will create the symlink ~/.vim/vim -> $DIR/vim
    (i.e. $DIR/vim/vim -> $DIR/vim). This was because the command
    `ln -s $SOURCE $NONEXISTENT_FOLDER` would create a symlink called
    $NONEXISTENT_FOLDER, but `ln -s $SOURCE $EXISTING_FOLDER` would create a symlink
    >in< $EXISTING_FOLDER rather than failing due to existence as with files. The
    fix was to simply check for existence before running the ln command.

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for file in `ls $DIR | grep -v "\.sh"`;
 do
-	ln -s $DIR/$file ~/.$file
+	if [ -e "$HOME/.$file" ];
+	then
+		echo "Skipping $HOME/.$file: file exists"
+	else
+		echo "Symlinking $HOME/.$file to $Dir/$file"
+		ln -s "$DIR/$file" "$HOME/.$file"
+	fi
 done
+echo "Installation complete"
